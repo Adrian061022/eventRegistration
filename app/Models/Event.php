5fe16ef1ad6250dfb05c85ab40ee410a68a9fc32
Márki Zoltán <1tc-marzol@moriczref.lan>
2026-01-08 09:10:04 +0100

    modellek es migraciok

---
+++ app/Models/Event.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Testing\Fluent\Concerns\Has;
+
+class Event extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'date',
+        'location',
+        'max_attendees',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'date' => 'datetime',
+        ];
+    }
+
+    public function registrations()
+    {
+        return $this->hasMany(Registration::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'registrations')
+        ->withPivot('status', 'registered_at')
+        ->withTimestamps();
+    }
+}
